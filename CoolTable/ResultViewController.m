9826b5187417d536113d09617714e4d270a1639d
oooffspring <o.offspring131@gmail.com>
2013-03-26 19:36:48 +0800

    change name

--- CoolTable/ResultViewController.m
+++ CoolTable/ResultViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ResultViewController.h"
+#import "QBFlatButton.h"
 
 @interface ResultViewController ()
+
+@property (nonatomic ,strong) QBFlatButton *btn;
 
 @end
 
@@ -28,7 +31,26 @@
     [super viewDidLoad];
     self.title = @"摇饭结果";
     self.diskName.text = self.diskNameString;
+    //自定义按钮
+    self.btn = [QBFlatButton buttonWithType:UIButtonTypeCustom];
+    self.btn.frame = CGRectMake(60, 290, 200, 60);
+    self.btn.faceColor = [UIColor redColor];
+    self.btn.sideColor = [UIColor colorWithRed:150.0/255.0 green:0.0/255.0 blue:0.0/255.0 alpha:1.0];
+    self.btn.radius = 6.0;
+    self.btn.margin = 7.0;
+    self.btn.depth = 6.0;
+    [self.btn.titleLabel setTextAlignment:NSTextAlignmentCenter];
+    [self.btn setTitle:@"返回首页继续摇" forState:UIControlStateNormal];
+    [self.btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.btn addTarget:self action:@selector(doneShaking:) forControlEvents:UIControlEventTouchUpInside];
+    self.btn.enabled = NO;
+    
+    [self.view addSubview:self.btn];
 	// Do any additional setup after loading the view.
+}
+
+-(void)doneShaking:(id)sender{
+    [self.navigationController popViewControllerAnimated:YES];
 }
 
 - (void)didReceiveMemoryWarning
