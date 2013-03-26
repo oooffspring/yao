baf5c675e1597bf016fc9754fd362ce40fe84a08
oooffspring <o.offspring131@gmail.com>
2013-03-27 05:36:05 +0800

    again

--- CoolTable/ResultViewController.m
+++ CoolTable/ResultViewController.m
@@ -31,11 +31,12 @@
     [super viewDidLoad];
     self.title = @"摇饭结果";
     self.diskName.text = self.diskNameString;
+    
     //自定义按钮
     self.btn = [QBFlatButton buttonWithType:UIButtonTypeCustom];
-    self.btn.frame = CGRectMake(60, 290, 200, 60);
-    self.btn.faceColor = [UIColor redColor];
-    self.btn.sideColor = [UIColor colorWithRed:150.0/255.0 green:0.0/255.0 blue:0.0/255.0 alpha:1.0];
+    self.btn.frame = CGRectMake(60, 250, 200, 60);
+    self.btn.faceColor = [UIColor lightGrayColor];
+    self.btn.sideColor = [UIColor darkGrayColor];
     self.btn.radius = 6.0;
     self.btn.margin = 7.0;
     self.btn.depth = 6.0;
@@ -43,9 +44,16 @@
     [self.btn setTitle:@"返回首页继续摇" forState:UIControlStateNormal];
     [self.btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [self.btn addTarget:self action:@selector(doneShaking:) forControlEvents:UIControlEventTouchUpInside];
-    self.btn.enabled = NO;
+    self.btn.enabled = YES;
     
     [self.view addSubview:self.btn];
+    
+    UIBezierPath *roundedRectanglePath = [UIBezierPath bezierPathWithRoundedRect: CGRectMake(5, 5, 100, 100) cornerRadius: 4];
+    [[UIColor whiteColor] setFill];
+    [roundedRectanglePath fill];
+    [[UIColor blackColor] setStroke];
+    roundedRectanglePath.lineWidth = 2;
+    [roundedRectanglePath stroke];
 	// Do any additional setup after loading the view.
 }
 
